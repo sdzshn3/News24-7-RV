3ae3eeb6aeeb64291913bfb71e7597541136a6bf
Mahima <mahima.6.ms@gmail.com>
2019-01-05 11:43:27 +0530

    add okhttp logging interceptor to log retrofit request responses

--- app/src/main/java/com/sdzshn3/android/news247/Retrofit/Client.java
+++ app/src/main/java/com/sdzshn3/android/news247/Retrofit/Client.java
@@ -2,16 +2,32 @@ package com.sdzshn3.android.news247.Retrofit;
 
 import com.sdzshn3.android.news247.SupportClasses.DataHolder;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Client {
 
+    private static Retrofit retrofit;
+
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    private static HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+
     private static Retrofit getRetrofitInstance() {
-        return new Retrofit.Builder()
-                .baseUrl(DataHolder.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+
+        if (retrofit == null) {
+
+            httpClient.addInterceptor(loggingInterceptor);
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(DataHolder.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
+                    .build();
+        }
+
+        return retrofit;
     }
 
     public static ApiService getApiService() {
